609dce9b7148194786d399a6e92ad9ea43abc697
Leng <lengzhang@live.com>
2021-04-22 22:22:25 -0700

    set app to be full-screen and no title bar

--- app/src/main/java/com/lengzhang/android/lz2048/MainActivity.kt
+++ app/src/main/java/com/lengzhang/android/lz2048/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.lengzhang.android.lz2048
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Tells Android that the app will be a full-screen app
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        // Indicates that should not be a title bar for the app
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+
         setContentView(R.layout.activity_main)
     }
 }
